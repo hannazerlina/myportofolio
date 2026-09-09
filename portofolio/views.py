73641e43883c55c1179108e41dbb6df1d2a0bf5d
Hanna <hannazerlina@gmail.com>
2026-09-09 20:26:22 +0700

    Update index.html with Django template variables and views context

--- portofolio/views.py
+++ portofolio/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+def show_main(request): # atau nama fungsi view profil kamu
+    context = {
+        'name': 'Hanna Zerlina Razaq Putri Wicaksono',
+        'npm': '2506594692',
+        'study_program': 'Sistem Informasi',
+        'bio': 'Isi bio singkat kamu di sini...',
+    }
+    return render(request, "index.html", context)
 
-def landing_page(request):
-    return render(request, "index.html")
