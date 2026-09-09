fd01f86eaa94e91f9c6510b3de2a9210e304c987
Hanna <hannazerlina@gmail.com>
2026-09-09 19:58:59 +0700

    Add Experience model and initial migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,26 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Experience(models.Model):
+    EXPERIENCE_CHOICES = [
+        ('internship', 'Internship'),
+        ('research', 'Research'),
+        ('volunteer', 'Volunteer'),
+        ('part-time', 'Part-Time'),
+        ('full-time', 'Full-Time'),
+        ('freelance', 'Freelance'),
+    ]
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.TextField()
+    started_at = models.DateField()
+    ended_at = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    @property
+    def is_ongoing(self):
+        return self.ended_at is None
